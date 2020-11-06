94885668140f540ae20de939090918a061a8c5e8
bandstrar <rob.bandstra@gmail.com>
2020-11-05 19:15:51 -0600

    made app that shows jokes on click of buttons

---
+++ src/components/App/index.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import getJokes from '../../helpers/data/getJokes';
+import Setup from '../Setup';
+import Punchline from '../Punchline';
+import jokeShape from '../../helpers/propz/jokeShape';
+import '../../App/App.scss';
+
+class App extends Component {
+  static propTypes = {
+    joke: jokeShape,
+  }
+
+    state = {
+      joke: {},
+      showSetup: false,
+      showPunchline: false,
+    }
+
+    componentDidMount() {
+      getJokes.getJokes().then((resp) => {
+        this.setState({
+          joke: resp,
+        });
+      });
+    }
+
+    showSetupJoke = () => {
+      this.setState({
+        showSetup: true,
+      });
+    }
+
+    showPunchlineJoke = () => {
+      this.setState({
+        showPunchline: true,
+      });
+    }
+
+    resetJoke = () => {
+      getJokes.getJokes().then((resp) => {
+        this.setState({
+          joke: resp,
+          showSetup: false,
+          showPunchline: false,
+        });
+      });
+    }
+
+    render() {
+      const { showSetup, showPunchline, joke } = this.state;
+      return (
+            <div className="App">
+      <img src="https://user-images.githubusercontent.com/29741570/98047811-372e3b80-1df2-11eb-9bb6-3e8845e92d9e.png" className="card-img-top" alt="..." />
+      {showSetup ? (<Setup setup={joke.setup}/>) : (null)}
+      {showPunchline ? (<Punchline punchline={joke.punchline}/>) : (null)}
+      {!showSetup && !showPunchline ? (
+        <button className='btn btn-dark setup' onClick={this.showSetupJoke}>Get a Joke</button>
+      ) : (null)}
+      {showSetup && !showPunchline ? (<button className='btn btn-dark punchline' onClick={this.showPunchlineJoke}>Get Punchline</button>) : (null)}
+        {showPunchline ? (<button className='btn btn-dark newjoke' onClick={this.resetJoke}>Get a New Joke</button>) : (null)}
+            </div>
+      );
+    }
+}
+
+export default App;
---
+++ src/components/Punchline/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Punchline extends Component {
+  render() {
+    // const { punchline } = this.props.punchline;
+    return (
+        <div>
+        <p>{this.props.punchline}</p>
+      </div>);
+  }
+}
+
+export default Punchline;
